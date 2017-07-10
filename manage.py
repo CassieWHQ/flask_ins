8b32dd7604e6afb043671c9999fd05859c3bfad4
CassieWHQ <13260597822@163.com>
2017-07-10 09:10:14 +0800

    second commit

---
+++ manage.py
@@ -0,0 +1,24 @@
+# -*- encoding :utf-8 -*-
+from flask_ins import app,db
+from flask_script import Manager
+from flask_ins.models import User,Image,Comment
+import random
+manager = Manager(app)
+
+def get_image_url():
+    return 'http://images.nowcoder.com/head/'+str(random.randint(0,1000))+'m.png'
+@manager.command
+def init_database():
+    db.drop_all()
+    db.create_all()
+    for i in range(0,100):
+        db.session.add(User('User'+str(i),'a'+str(i)))#username,password
+        for j in range(0,3):
+            db.session.add(Image(get_image_url(),i+1))
+            for k in range(0,3):
+                db.session.add(Comment('comment'+str(k),1+3*i+j,i+1))
+
+
+    db.session.commit()
+if __name__ == '__main__':
+    manager.run()
